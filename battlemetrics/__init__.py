2eb6868ceb46c3bc70bce92fe9370ebeb85641d5
OseSem <sem@osesem.me>
2024-11-19 21:10:57 +0100

    Remove weird versioning

--- battlemetrics/__init__.py
+++ battlemetrics/__init__.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Final, Literal, NamedTuple
+from typing import Final
 
 from .client import *
 from .errors import *
@@ -10,21 +10,6 @@ from .types import *
 
 # Placeholder, modified by dynamic-versioning.
 __version__: Final[str] = "0.0.0"
-
-
-class VersionInfo(NamedTuple):
-    """Represents the version information of the library."""
-
-    major: int
-    minor: int
-    micro: int
-    releaselevel: Literal["alpha", "beta", "candidate", "final"]
-    serial: int
-    metadata: str
-
-
-# Placeholder, modified by dynamic-versioning.
-version_info: VersionInfo = VersionInfo(0, 0, 0, "final", 0, "")
 
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
