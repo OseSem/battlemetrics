3510062f31499443d4e225b1b4b3740d6e2ca539
OseSem <sem@osesem.me>
2024-11-19 18:18:28 +0100

    Add poetry-dynamic-versioning

--- battlemetrics/__init__.py
+++ battlemetrics/__init__.py
@@ -1,5 +1,5 @@
-import logging as _logging
-from typing import Final
+import logging
+from typing import Final, Literal, NamedTuple
 
 from .client import *
 from .errors import *
@@ -8,6 +8,23 @@ from .note import *
 from .server import *
 from .types import *
 
-__version__: Final[str] = "2.0"
+# Placeholder, modified by dynamic-versioning.
+__version__: Final[str] = "0.0.0"
 
-_logging.getLogger(__name__).addHandler(_logging.NullHandler())
+
+class VersionInfo(NamedTuple):
+    """Represents the version information of the library."""
+
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+    metadata: str
+
+
+# Placeholder, modified by dynamic-versioning.
+version_info: VersionInfo = VersionInfo(0, 0, 0, "final", 0, "")
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
